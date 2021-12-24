171db5b2e7a93f9521bb0626de5a774e89b1da81
justxyx <635893003@qq.com>
2021-12-24 19:39:31 +0800

    more

---
+++ 01more.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+#define PAGELEN 24
+#define LINELEN 512
+
+void do_more(FILE *);
+int see_more();
+
+/**
+ * 1.我们首先要清楚unix/linux下的系统编程，main函数上面是有参数的，即main（int ac，char *av[]），
+ * 这个带参数是干啥的呢？第一个ac是表示程序运行时输入的参数个数（int型），第二个是一个指针数组，表示参数的符号。举个例子：
+
+./more01 执行之后，ac=1，av[0]=./more01，./more01 more01.c执行之后，ac=2，av[0]=./more01,av[1]=more01.c
+ 如果执行./more01 -f more01.c 的话，ac=3，av[0]=./more01,av[1]=-f ，av[2]=more01.c，然后至于怎么实现的这样的功能的，
+ 这里不是讨论的重点，知道就好。
+
+ * @param ac
+ * @param av
+ * @return
+ */
+
+int main( int ac ,char *av[]){
+    FILE *fp;
+    if  ( ac == 1)
+        do_more(fp);
+    else
+        while (--ac){
+            if( (fp = fopen(*++av,"r")) != NULL){
+                do_more(fp);
+                fclose(fp);
+            } else
+            {
+
+            }
+
+        }
+    return 0;
+}
+
+void do_more( FILE *fp )
+/*
+ *  read PAGELEN lines, then call see_more() for further instructions
+ */
+{
+    char	line[LINELEN];
+    int	num_of_lines = 0;
+    int	see_more(), reply;
+
+    while ( fgets( line, LINELEN, fp ) ){		/* more input	*/
+        if ( num_of_lines == PAGELEN ) {	/* full screen?	*/
+            reply = see_more();		/* y: ask user  */
+            if ( reply == 0 )		/*    n: done   */
+                break;
+            num_of_lines -= reply;		/* reset count	*/
+        }
+        if ( fputs( line, stdout )  == EOF )	/* show line	*/
+            return;			/* or die	*/
+        num_of_lines++;				/* count it	*/
+    }
+}
+
+int see_more(){
+    int c;
+    printf("\\033[7m more? \\033[m");
+    while ((c= getchar()) != EOF){
+        if  ( c == 'q')
+            return 0;
+        if  ( c == ' ')
+            return PAGELEN;
+        if  ( c == '\n')
+            return 1;
+    }
+    return 0;
+}
